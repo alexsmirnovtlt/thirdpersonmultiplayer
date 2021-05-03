95fe5f6dd8fc66cc1b0c393e873af809efccfecb
Alexander Smirnov <alexsmirnovtlt@gmail.com>
2021-05-03 12:44:18 +0400

    minor fixes

--- Source/tpmultiplayer/Public/General/Animation/StepAnimNotify_FMOD.h
+++ Source/tpmultiplayer/Public/General/Animation/StepAnimNotify_FMOD.h
@@ -3,14 +3,13 @@
 #pragma once
 
 #include "FMODEvent.h"
-#include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotify.h"
 #include "StepAnimNotify_FMOD.generated.h"
 
 /**
  * Slightly modified version of UFMODAnimNotifyPlay that can play random step sound at bone location
  */
-UCLASS(abstract, const, hidecategories = Object, collapsecategories, meta = (DisplayName = "Play FMOD Event"))
+UCLASS(abstract, const, hidecategories = Object, collapsecategories, meta = (DisplayName = "Play FMOD Step Event"))
 class TPMULTIPLAYER_API UStepAnimNotify_FMOD : public UAnimNotify
 {
 	GENERATED_BODY()
@@ -20,6 +19,7 @@ public:
 
     // Being UAnimNotify interface
     virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* AnimSeq) override;
+    virtual FString GetNotifyName_Implementation() const override { return Super::GetNotifyName_Implementation(); };
     // End UAnimNotify interface
 
     // Socket or bone name to attach sound to
